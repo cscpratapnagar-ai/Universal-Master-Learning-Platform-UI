0ae47c6d2ad466af00a9a778920d8710929a17a5
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-08-30 12:47:18 +0530

    feat(organizations): register organization management module

--- src/app/features/super-admin/super-admin.module.ts
+++ src/app/features/super-admin/super-admin.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { SuperAdminRoutingModule } from './super-admin-routing.module';
 import { SuperAdminDashboardComponent } from './pages/super-admin-dashboard/super-admin-dashboard.component';
+import { OrganizationManagementComponent } from './pages/organization-management/organization-management.component';
 
 @NgModule({
-  declarations: [SuperAdminDashboardComponent],
+  declarations: [SuperAdminDashboardComponent, OrganizationManagementComponent],
   imports: [CommonModule, SharedModule, SuperAdminRoutingModule]
 })
 export class SuperAdminModule {}
