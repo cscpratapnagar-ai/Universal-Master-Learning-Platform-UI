c285c2eb67f1b0767065bac2aefe7a45d3d92361
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-08-30 12:58:09 +0530

    refactor(super-admin): declare shell layout component

--- src/app/features/super-admin/super-admin.module.ts
+++ src/app/features/super-admin/super-admin.module.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../../shared/shared.module';
 import { SuperAdminRoutingModule } from './super-admin-routing.module';
+import { SuperAdminShellComponent } from './layout/super-admin-shell.component';
 import { SuperAdminDashboardComponent } from './pages/super-admin-dashboard/super-admin-dashboard.component';
 import { OrganizationManagementComponent } from './pages/organization-management/organization-management.component';
 
 @NgModule({
-  declarations: [SuperAdminDashboardComponent, OrganizationManagementComponent],
-  imports: [CommonModule, FormsModule, SharedModule, SuperAdminRoutingModule]
+  declarations: [SuperAdminShellComponent, SuperAdminDashboardComponent, OrganizationManagementComponent],
+  imports: [CommonModule, FormsModule, RouterModule, SharedModule, SuperAdminRoutingModule]
 })
 export class SuperAdminModule {}
