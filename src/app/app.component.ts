4a24604043183c822e25f7fba9bf7accec0c7fa2
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-08-30 23:33:47 +0530

    feat(session): restore authentication session on application startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Universal Master Learning Platform';
+
+  constructor(private readonly authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.restoreSession().subscribe();
+  }
 }
