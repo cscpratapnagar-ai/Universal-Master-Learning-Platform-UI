4c34d684f8a81b46bd90ea392f82596f0c523891
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-08-30 12:47:22 +0530

    feat(organizations): add organization management route

--- src/app/features/super-admin/super-admin-routing.module.ts
+++ src/app/features/super-admin/super-admin-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { SuperAdminDashboardComponent } from './pages/super-admin-dashboard/super-admin-dashboard.component';
+import { OrganizationManagementComponent } from './pages/organization-management/organization-management.component';
 
 const routes: Routes = [
-  { path: '', component: SuperAdminDashboardComponent }
+  { path: '', component: SuperAdminDashboardComponent },
+  { path: 'organizations', component: OrganizationManagementComponent }
 ];
 
 @NgModule({
