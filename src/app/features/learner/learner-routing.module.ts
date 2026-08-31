afe2a1015946d98eb8605a5e79fb77f7ed43ab9a
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-08-31 15:54:45 +0530

    feat(learner): add real learning pages and routes

--- src/app/features/learner/learner-routing.module.ts
+++ src/app/features/learner/learner-routing.module.ts
@@ -1,19 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { AuthGuard } from '../../core/guards/auth.guard';
 import { LearnerDashboardComponent } from './pages/learner-dashboard/learner-dashboard.component';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: LearnerDashboardComponent,
-    canActivate: [AuthGuard]
-  }
-];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
-export class LearnerRoutingModule {}
+import { MyCoursesComponent } from './pages/my-courses/my-courses.component';
+import { CoursePlayerComponent } from './pages/course-player/course-player.component';
+const routes:Routes=[{path:'',component:LearnerDashboardComponent,canActivate:[AuthGuard]},{path:'courses',component:MyCoursesComponent,canActivate:[AuthGuard]},{path:'course/:enrollmentId',component:CoursePlayerComponent,canActivate:[AuthGuard]}];
+@NgModule({imports:[RouterModule.forChild(routes)],exports:[RouterModule]}) export class LearnerRoutingModule {}
