8a8fbfa4c82741a7efd95b1ee59be85c769f32cc
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-08-31 07:36:45 +0530

    feat(super-admin): register users management page

--- src/app/features/super-admin/super-admin.module.ts
+++ src/app/features/super-admin/super-admin.module.ts
@@ -8,9 +8,15 @@ import { SuperAdminRoutingModule } from './super-admin-routing.module';
 import { SuperAdminShellComponent } from './layout/super-admin-shell.component';
 import { SuperAdminDashboardComponent } from './pages/super-admin-dashboard/super-admin-dashboard.component';
 import { OrganizationManagementComponent } from './pages/organization-management/organization-management.component';
+import { UserManagementComponent } from './pages/user-management/user-management.component';
 
 @NgModule({
-  declarations: [SuperAdminShellComponent, SuperAdminDashboardComponent, OrganizationManagementComponent],
+  declarations: [
+    SuperAdminShellComponent,
+    SuperAdminDashboardComponent,
+    OrganizationManagementComponent,
+    UserManagementComponent
+  ],
   imports: [CommonModule, FormsModule, RouterModule, SharedModule, SuperAdminRoutingModule]
 })
 export class SuperAdminModule {}
