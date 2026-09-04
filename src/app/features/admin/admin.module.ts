76ff45934827e2b37c4a21738e5e8aa93c3b8aa4
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-05 01:18:36 +0530

    feat: register curriculum studio

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -3,10 +3,11 @@ import { SharedModule } from '../../shared/shared.module';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
 import { AssessmentBuilderComponent } from './pages/assessment-builder/assessment-builder.component';
+import { CurriculumComponent } from './pages/curriculum/curriculum.component';
 import { LearningPathComponent } from './pages/learning-path/learning-path.component';
 
 @NgModule({
-  declarations: [AdminDashboardComponent, AssessmentBuilderComponent, LearningPathComponent],
+  declarations: [AdminDashboardComponent, AssessmentBuilderComponent, CurriculumComponent, LearningPathComponent],
   imports: [SharedModule, AdminRoutingModule]
 })
 export class AdminModule {}
