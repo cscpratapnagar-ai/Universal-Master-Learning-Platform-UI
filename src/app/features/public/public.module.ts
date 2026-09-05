6f778a7764b4e91c9622185ce9a0266c4f1389ef
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-05 23:25:33 +0530

    feat: register architecture page

--- src/app/features/public/public.module.ts
+++ src/app/features/public/public.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { PublicRoutingModule } from './public-routing.module';
 import { LandingComponent } from './pages/landing/landing.component';
+import { ArchitectureComponent } from './pages/architecture/architecture.component';
 
 @NgModule({
-  declarations: [LandingComponent],
+  declarations: [LandingComponent, ArchitectureComponent],
   imports: [SharedModule, PublicRoutingModule]
 })
 export class PublicModule {}
