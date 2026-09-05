8b66f63a9da9b87cb3bfa9487c5db8efbfaacc06
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-05 23:25:24 +0530

    feat: route master learning architecture page

--- src/app/features/public/public-routing.module.ts
+++ src/app/features/public/public-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LandingComponent } from './pages/landing/landing.component';
+import { ArchitectureComponent } from './pages/architecture/architecture.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LandingComponent
+  },
+  {
+    path: 'architecture',
+    component: ArchitectureComponent
   }
 ];
 
