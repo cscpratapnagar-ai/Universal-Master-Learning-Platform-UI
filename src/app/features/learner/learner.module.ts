01e0df39709ef75ee63cf58cd9c81e2165265894
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-14 23:23:27 +0530

    register learner profile page

--- src/app/features/learner/learner.module.ts
+++ src/app/features/learner/learner.module.ts
@@ -1,13 +1,4 @@
 import { NgModule } from '@angular/core';
-import { SharedModule } from '../../shared/shared.module';
-import { LearnerRoutingModule } from './learner-routing.module';
-import { LearnerDashboardComponent } from './pages/learner-dashboard/learner-dashboard.component';
-import { MyCoursesComponent } from './pages/my-courses/my-courses.component';
-import { CourseDetailsComponent } from './pages/course-details/course-details.component';
-import { CoursePlayerComponent } from './pages/course-player/course-player.component';
-import { ProgressAnalyticsComponent } from './pages/progress-analytics/progress-analytics.component';
-import { QuizComponent } from './pages/quiz/quiz.component';
-import { AssessmentResultComponent } from './pages/assessment-result/assessment-result.component';
-import { CertificateComponent } from './pages/certificate/certificate.component';
-@NgModule({declarations:[LearnerDashboardComponent,MyCoursesComponent,CourseDetailsComponent,CoursePlayerComponent,ProgressAnalyticsComponent,QuizComponent,AssessmentResultComponent,CertificateComponent],imports:[SharedModule,LearnerRoutingModule]})
-export class LearnerModule{}
+import { SharedModule } from '../../shared/shared.module';import { LearnerRoutingModule } from './learner-routing.module';
+import { LearnerDashboardComponent } from './pages/learner-dashboard/learner-dashboard.component';import { MyCoursesComponent } from './pages/my-courses/my-courses.component';import { CourseDetailsComponent } from './pages/course-details/course-details.component';import { CoursePlayerComponent } from './pages/course-player/course-player.component';import { ProgressAnalyticsComponent } from './pages/progress-analytics/progress-analytics.component';import { ProfileComponent } from './pages/profile/profile.component';import { QuizComponent } from './pages/quiz/quiz.component';import { AssessmentResultComponent } from './pages/assessment-result/assessment-result.component';import { CertificateComponent } from './pages/certificate/certificate.component';
+@NgModule({declarations:[LearnerDashboardComponent,MyCoursesComponent,CourseDetailsComponent,CoursePlayerComponent,ProgressAnalyticsComponent,ProfileComponent,QuizComponent,AssessmentResultComponent,CertificateComponent],imports:[SharedModule,LearnerRoutingModule]}) export class LearnerModule{}
