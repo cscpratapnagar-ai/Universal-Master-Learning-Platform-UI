b27d5edde6f24e85c9868854d5bae7bc00ca346e
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-14 22:57:08 +0530

    add course details and learning routes

--- src/app/features/learner/learner-routing.module.ts
+++ src/app/features/learner/learner-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../../core/guards/auth.guard';
 import { LearnerDashboardComponent } from './pages/learner-dashboard/learner-dashboard.component';
 import { MyCoursesComponent } from './pages/my-courses/my-courses.component';
+import { CourseDetailsComponent } from './pages/course-details/course-details.component';
 import { CoursePlayerComponent } from './pages/course-player/course-player.component';
 import { QuizComponent } from './pages/quiz/quiz.component';
 import { AssessmentResultComponent } from './pages/assessment-result/assessment-result.component';
@@ -11,14 +12,12 @@ import { CertificateComponent } from './pages/certificate/certificate.component'
 const routes: Routes = [
   { path: '', component: LearnerDashboardComponent, canActivate: [AuthGuard] },
   { path: 'courses', component: MyCoursesComponent, canActivate: [AuthGuard] },
-  { path: 'course/:enrollmentId', component: CoursePlayerComponent, canActivate: [AuthGuard] },
+  { path: 'course/:enrollmentId', component: CourseDetailsComponent, canActivate: [AuthGuard] },
+  { path: 'course/:enrollmentId/learn', component: CoursePlayerComponent, canActivate: [AuthGuard] },
   { path: 'quiz', component: QuizComponent, canActivate: [AuthGuard] },
   { path: 'assessment-result', component: AssessmentResultComponent, canActivate: [AuthGuard] },
   { path: 'certificates', component: CertificateComponent, canActivate: [AuthGuard] }
 ];
 
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
+@NgModule({ imports: [RouterModule.forChild(routes)], exports: [RouterModule] })
 export class LearnerRoutingModule {}
