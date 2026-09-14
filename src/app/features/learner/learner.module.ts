6821bf06bde20b784e640d1d73c528a04ef3beeb
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-14 22:56:57 +0530

    register course details page

--- src/app/features/learner/learner.module.ts
+++ src/app/features/learner/learner.module.ts
@@ -3,20 +3,14 @@ import { SharedModule } from '../../shared/shared.module';
 import { LearnerRoutingModule } from './learner-routing.module';
 import { LearnerDashboardComponent } from './pages/learner-dashboard/learner-dashboard.component';
 import { MyCoursesComponent } from './pages/my-courses/my-courses.component';
+import { CourseDetailsComponent } from './pages/course-details/course-details.component';
 import { CoursePlayerComponent } from './pages/course-player/course-player.component';
 import { QuizComponent } from './pages/quiz/quiz.component';
 import { AssessmentResultComponent } from './pages/assessment-result/assessment-result.component';
 import { CertificateComponent } from './pages/certificate/certificate.component';
 
 @NgModule({
-  declarations: [
-    LearnerDashboardComponent,
-    MyCoursesComponent,
-    CoursePlayerComponent,
-    QuizComponent,
-    AssessmentResultComponent,
-    CertificateComponent
-  ],
+  declarations: [LearnerDashboardComponent, MyCoursesComponent, CourseDetailsComponent, CoursePlayerComponent, QuizComponent, AssessmentResultComponent, CertificateComponent],
   imports: [SharedModule, LearnerRoutingModule]
 })
 export class LearnerModule {}
