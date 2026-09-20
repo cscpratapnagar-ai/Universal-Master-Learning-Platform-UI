430c02fa7cc1ca7e6c4b5b3ba792cdc3676f545a
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-20 11:44:21 +0530

    feat: register teacher course studio

--- src/app/features/teacher/teacher.module.ts
+++ src/app/features/teacher/teacher.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { TeacherRoutingModule } from './teacher-routing.module';
 import { TeacherDashboardComponent } from './pages/teacher-dashboard/teacher-dashboard.component';
+import { TeacherCoursesComponent } from './pages/teacher-courses/teacher-courses.component';
 
 @NgModule({
-  declarations: [TeacherDashboardComponent],
+  declarations: [TeacherDashboardComponent, TeacherCoursesComponent],
   imports: [SharedModule, TeacherRoutingModule]
 })
 export class TeacherModule {}
