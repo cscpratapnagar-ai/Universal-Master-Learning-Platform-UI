5ad19856bcad1731a7a31d4e1cbb442c34082db7
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-24 12:04:39 +0530

    Register teacher learner analytics page

--- src/app/features/teacher/teacher.module.ts
+++ src/app/features/teacher/teacher.module.ts
@@ -5,9 +5,10 @@ import { TeacherDashboardComponent } from './pages/teacher-dashboard/teacher-das
 import { TeacherCoursesComponent } from './pages/teacher-courses/teacher-courses.component';
 import { TeacherAssessmentsComponent } from './pages/teacher-assessments/teacher-assessments.component';
 import { TeacherQuestionBankComponent } from './pages/teacher-question-bank/teacher-question-bank.component';
+import { TeacherLearnersComponent } from './pages/teacher-learners/teacher-learners.component';
 
 @NgModule({
-  declarations: [TeacherDashboardComponent, TeacherCoursesComponent, TeacherAssessmentsComponent, TeacherQuestionBankComponent],
+  declarations: [TeacherDashboardComponent, TeacherCoursesComponent, TeacherAssessmentsComponent, TeacherQuestionBankComponent, TeacherLearnersComponent],
   imports: [SharedModule, TeacherRoutingModule]
 })
 export class TeacherModule {}
